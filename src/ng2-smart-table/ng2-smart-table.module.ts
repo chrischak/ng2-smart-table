cb5cdc3b8d84a10f5501863be6d6e3d98a259650
Mikhail <m@ovchinnikov.msk.ru>
2017-04-05 21:21:15 +0300

    Merge with upstream

---
+++ src/ng2-smart-table/ng2-smart-table.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CellModule } from './components/cell/cell.module';
+import { FilterModule } from './components/filter/filter.module';
+import { PagerModule } from './components/pager/pager.module';
+import { TBodyModule } from './components/tbody/tbody.module';
+import { THeadModule } from './components/thead/thead.module';
+
+import { Ng2SmartTableComponent } from './ng2-smart-table.component';
+
+import { DragulaModule } from 'ng2-dragula';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CellModule,
+    FilterModule,
+    PagerModule,
+    TBodyModule,
+    THeadModule,
+    
+    DragulaModule,
+  ],
+  declarations: [
+    Ng2SmartTableComponent,
+  ],
+  exports: [
+    Ng2SmartTableComponent,
+  ],
+})
+export class Ng2SmartTableModule {
+}
